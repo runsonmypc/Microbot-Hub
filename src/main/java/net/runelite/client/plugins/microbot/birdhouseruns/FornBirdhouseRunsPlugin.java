d0c98f38fe90e9e73d480755af3b91b0c3919e9b
runsonmypc <runsonmypc@users.noreply.github.com>
2026-05-26 17:42:31 -0400

    chore(birdhouse): bump version to 1.1.4

--- src/main/java/net/runelite/client/plugins/microbot/birdhouseruns/FornBirdhouseRunsPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/birdhouseruns/FornBirdhouseRunsPlugin.java
@@ -26,7 +26,7 @@ import java.awt.*;
 )
 @Slf4j
 public class FornBirdhouseRunsPlugin extends Plugin {
-    final static String version = "1.1.3";
+    final static String version = "1.1.4";
     @Provides
     FornBirdhouseRunsConfig provideConfig(ConfigManager configManager) {
         return configManager.getConfig(FornBirdhouseRunsConfig.class);
